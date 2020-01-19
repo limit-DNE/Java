45c06880bf8b9ff93bc88e7a682a2cc43449d69f
Iris <44437616+limit-DNE@users.noreply.github.com>
2020-01-19 13:40:17 +0300

    Add files via upload

---
+++ JavaFX/Loan Calculator/src/module-info.java
@@ -0,0 +1,10 @@
+module Loan.Calculator {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.base;
+    requires javafx.graphics;
+    requires javafx.media;
+    requires javafx.swing;
+    requires javafx.web;
+    opens sample;
+}
